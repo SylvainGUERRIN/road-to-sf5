259695487beb9f182745bd01c9c5a77852178766
sguerrin549@gmail.com <ganon1983>
2020-03-19 14:18:04 +0100

    modif conferenceRepository

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -19,32 +19,11 @@ class ConferenceRepository extends ServiceEntityRepository
         parent::__construct($registry, Conference::class);
     }
 
-    // /**
-    //  * @return Conference[] Returns an array of Conference objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Conference[]|array
+     */
+    public function findAll(): array
     {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+        return $this->findBy([], ['year'=>'ASC', 'city'=>'ASC']);
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Conference
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
