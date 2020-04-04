52cfcf81f1107944485b3db62448ba1357f7df7f
sguerrin549@gmail.com <ganon1983>
2020-04-04 13:42:59 +0200

    add templates and styles for spa

---
+++ spa/src/app.js
@@ -0,0 +1,36 @@
+import '../assets/css/app.scss';
+import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+import Home from './pages/home';
+import Conference from './pages/conference';
+
+function App(){
+    return(
+        <div>
+            <header className="header">
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container">
+                        <Link className="navbar-brand mr-4 pr-2" href="/">
+                            &#128217; Guestbook
+                        </Link>
+                    </div>
+                </nav>
+
+                <nav className="bg-light border-bottom text-center">
+                    <div className="container">
+                        <Link className="nav-conference" href="/conference/amsterdam2019">
+                            Amsterdam 2019
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+
+            <Router>
+                <Home path="/" />
+                <Conference path="/conference/:slug" />
+            </Router>
+        </div>
+    )
+}
+
+render(<App />, document.getElementById('app'));
